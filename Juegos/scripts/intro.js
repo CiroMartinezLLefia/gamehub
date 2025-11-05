8d5fde4cfd668d01a169ed40d329ccdc1e4ed35f
CiroMartinezLLefia <martinezmartinciro@fpllefia.com>
2025-11-05 14:52:33 +0000

    Click here

--- Juegos/scripts/intro.js
+++ Juegos/scripts/intro.js
@@ -5,5 +5,5 @@ document.addEventListener('DOMContentLoaded', () => {
         intro.style.transition = 'opacity 1s ease';
         intro.style.opacity = '0';
         setTimeout(() => intro.style.display = 'none', 1000);
-    }, 2000); // dura 2 segundos
+    }, 1000); // dura 1 segundos
 });
